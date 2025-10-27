04c10c05d66b5189111e89702fc2122d8c41b3c1
KalininSaveliy <kalinin.savelii@phystech.edu>
2025-10-27 23:08:33 +0300

    save gif flat

--- LiftingForce/make_graph/gif_flat.py
+++ LiftingForce/make_graph/gif_flat.py
@@ -6,25 +6,19 @@ from config.cfg import Config, path_list
 
 
 class Data:
-    def __init__(self, data_dir:str):
+    def __init__(self, cfg:Config):
         self.sep = ','
         self.header = None
-        self.data_dir = data_dir
+        self.data_dir = "data\\" + cfg.cfg["save_folder"] + "\\"
         self.search_data()
         self.read_coords()
         self.find_min_max_value()
 
     def search_data(self)->None:
-        self.data_list = path_list(self.data_dir, "*.csv")
-        grid_ind = -1
-        for i in range(len(self.data_list)):
-            if "grid.csv" in self.data_list[i]: 
-                grid_ind = i
-                break  # TODO: здесь можно проверить количество сеток и поймать ошибку
-        if grid_ind == -1:
-            raise Exception("Can't find grid file with coordinates")
-        self.grid_file = self.data_list.pop(grid_ind)
+        self.data_list = [self.data_dir + str(n) + ".csv" for n in range(0, cfg.cfg["n_t"], cfg.cfg["s_t"])]
         self.length = len(self.data_list)
+        print(self.data_list)
+        self.grid_file = self.data_dir + "grid.csv"
 
     def read_coords(self)->None:
         self.coords = dict()
@@ -55,14 +49,14 @@ cfg_name = "main"  # TODO: make it not hard const
 
 cfg = Config("config\\" + cfg_name + ".json")
 plate = [[0, cfg.cfg["real_plate_len"]], [0, 0]]
-data = Data("data\\" + cfg.cfg["save_folder"])
+data = Data(cfg)
 
 cbar = None
 fig, ax = plt.subplots()
 ax.plot(*plate, color="black", linewidth=2)
 
 def update(frame_ind):
-    print("smth ", frame_ind)
+    print("frame ", frame_ind)
     x = data.read(frame_ind)
     mesh = x.plot.pcolormesh(
         ax=ax,
@@ -77,9 +71,9 @@ def update(frame_ind):
     if frame_ind == 0:
         cbar = fig.colorbar(mesh, ax=ax)
         cbar.set_label('Value')
-    ax.set_title(frame_ind)
+    ax.set_title(frame_ind * cfg.cfg["s_t"])
 
 ani = FuncAnimation(fig=fig, func=update, frames=data.length, interval=1000, repeat=False, init_func=lambda : None)
-# ani.save(output_gif, writer='pillow', fps=fps) # fps = 5
+ani.save("graphs/eval.gif", writer='pillow', fps=1)
 
-plt.show()
+# plt.show()
